b5dac52aafff2f031cb005f4e6d05a37e064c269
Better～Me <zhangwenjie6885@163.com>
2019-06-26 23:30:20 +0800

    home--增加音频

--- src/views/home/drawInfo.vue
+++ src/views/home/drawInfo.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="pic_info" v-if="infoContent">
     <div class="wrap">
-        <h4>{{infoContent.title}}</h4>
-        <p>{{infoContent.time}}</p>
-        <div class="con" v-html="infoContent.text"></div>
+      <h4>{{infoContent.title}}</h4>
+      <p>{{infoContent.time}}</p>
+      <div v-if='infoContent.music'>
+        <player
+          :src="imgPre+infoContent.music"
+          :time="infoContent.music_time"
+          ref="player"
+        ></player>
+      </div>
+      <div class="con" v-html="infoContent.text"></div>
     </div>
   </div>
 </template>
@@ -11,7 +18,10 @@
 <script  lang='ts'>
 import AbstractBaseVue, { MyComponent, MyMixins } from "@/util/AbstractBaseVue";
 import { PictureApi } from "@/api/feature";
-@MyComponent
+import Player from "@/components/player.vue";
+@MyComponent({
+  components: { Player }
+})
 export default class PicInfo extends AbstractBaseVue {
   //生命周期 - 创建完成（可以访问当前this实例）
   infoContent: any = null; //详情页面得内容
